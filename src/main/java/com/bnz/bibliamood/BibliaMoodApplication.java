e6f7c05435243d24373e55b922fa55bd572a6b30
Benitonz <benzoni2009@gmail.com>
2025-09-08 00:25:05 +0200

    feat: init spring bath config + test

---
+++ src/main/java/com/bnz/bibliamood/BibliaMoodApplication.java
@@ -0,0 +1,11 @@
+package com.bnz.bibliamood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliaMoodApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BibliaMoodApplication.class, args);
+    }
+}
